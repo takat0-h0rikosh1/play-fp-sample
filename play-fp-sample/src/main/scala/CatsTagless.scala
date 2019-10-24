b57b484916d997738443c3a69a52c5e57889a4cb
堀越貴斗 <t_horikoshi@septeni-original.co.jp>
2019-10-24 18:49:10 +0900

    temp

--- play-fp-sample/src/main/scala/CatsTagless.scala
+++ play-fp-sample/src/main/scala/CatsTagless.scala
@@ -2,12 +2,13 @@ import cats.Applicative
 import cats.data.Kleisli
 import com.softwaremill.macwire._
 import monix.eval.Task
+import scalikejdbc.{AutoSession, DB, DBSession}
 
 import scala.concurrent.Await
 import scala.concurrent.duration._
 
 object Type {
-  type R[A] = Kleisli[Task, Unit, A]
+  type R[A] = Kleisli[Task, DBSession, A]
 }
 
 import Type._
@@ -27,17 +28,32 @@ trait DogRepository[F[_]] {
 }
 
 class CatRepositoryImpl extends CatRepository[R] {
-  override def resolveAll: R[Seq[Cat]] = Kleisli { _: Unit =>
+  override def resolveAll: R[Seq[Cat]] = Kleisli { implicit dbSession =>
     Task(Seq(Cat("たま")))
   }
 }
 class DogRepositoryImpl extends DogRepository[R] {
-  override def resolveAll: R[Seq[Dog]] = Kleisli { _: Unit =>
+  override def resolveAll: R[Seq[Dog]] = Kleisli { implicit dbSession =>
     Task(Seq(Dog("ぺろ")))
   }
 }
 
-// usecase
+class IOContextManagerOnJDBC extends IOContextManager[Task, DBSession] {
+
+  override def context: DBSession = AutoSession
+
+  override def transactionalContext[T](
+      execution: (DBSession) => Task[T]
+  ): Task[T] =
+    Task.deferFutureAction { implicit scheduler =>
+      DB.futureLocalTx { session =>
+        execution(session).runToFuture
+      }
+    }
+}
+
+// use case
+/*
 trait CatResolveUseCase[F[_]] {
   def resolveAll: F[Seq[Cat]]
 }
@@ -55,32 +71,45 @@ class DogResolveUseCaseImpl[F[_]](
 ) extends DogResolveUseCase[F] {
   def resolveAll: F[Seq[Dog]] = dogRepository.resolveAll
 }
+ */
 
 // service
 trait AnimalService[F[_]] {
   def resolveAll: F[Seq[Animal]]
 }
 class AnimalServiceImpl[F[_]: Applicative](
-    catResolveUseCase: CatResolveUseCase[F],
-    dogResolveUseCase: DogResolveUseCase[F]
+    catRepository: CatRepository[F],
+    dogRepository: DogRepository[F]
 ) extends AnimalService[F] {
   def resolveAll: F[Seq[Animal]] =
     Applicative[F].map2[Seq[Cat], Seq[Dog], Seq[Animal]](
-      catResolveUseCase.resolveAll,
-      dogResolveUseCase.resolveAll
+      catRepository.resolveAll,
+      dogRepository.resolveAll
     ) { (cats, dogs) =>
       cats ++ dogs
     }
 }
 
+trait IOContextManager[F[_], Ctx] {
+
+  def context: Ctx
+
+  def transactionalContext[T](execution: (Ctx) => F[T]): F[T]
+
+}
+
 object AnimalServiceComponent {
 
+  lazy val ioContext: IOContextManager[Task, DBSession] =
+    wire[IOContextManagerOnJDBC]
   lazy val catRepository: CatRepository[R] = wire[CatRepositoryImpl]
   lazy val dogRepository: DogRepository[R] = wire[DogRepositoryImpl]
+  /*
   lazy val catResolveUseCase: CatResolveUseCase[R] =
     wire[CatResolveUseCaseImpl[R]]
   lazy val dogResolveUseCase: DogResolveUseCase[R] =
     wire[DogResolveUseCaseImpl[R]]
+   */
   lazy val animalService: AnimalService[R] =
     wire[AnimalServiceImpl[R]]
 }
@@ -91,5 +120,8 @@ object Main {
   import AnimalServiceComponent._
 
   val animals: Seq[Animal] =
-    Await.result(animalService.resolveAll.run(()).runToFuture, 50.millisecond)
+    Await.result(
+      animalService.resolveAll.run((AutoSession)).runToFuture,
+      50.millisecond
+    )
 }
